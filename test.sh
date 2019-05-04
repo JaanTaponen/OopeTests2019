f64256096cbb868075483829f8c5464be2d390a0
Jaan Taponen <41044502+JaanTaponen@users.noreply.github.com>
2019-05-04 22:26:57 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,71 +1,71 @@
 #!/bin/bash
 
-echo "-----------cd1----------------"
+echo "-----------cd1-----------------------------------------------------------------------------------------------"
 java harjoitustyo.Oope2HT < esimerkit/in_cd1.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_cd1.txt -
 echo " "
 echo " "
 
-echo "-----------cd2----------------"
+echo "-----------cd2-----------------------------------------------------------------------------------------------"
 java harjoitustyo.Oope2HT < esimerkit/in_cd2.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_cd2.txt -
 echo " "
 echo " "
 
-echo "-----------exit1----------------"
+echo "-----------exit1-----------------------------------------------------------------------------------------------"
 java harjoitustyo.Oope2HT < esimerkit/in_exit.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_exit.txt -
 echo " "
 echo " "
 
-echo "-----------ls1----------------"
+echo "-----------ls1-----------------------------------------------------------------------------------------------"
 java harjoitustyo.Oope2HT < esimerkit/in_ls1.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_ls1.txt -
 echo " "
 echo " "
 
-echo "-----------ls2----------------"
+echo "-----------ls2-----------------------------------------------------------------------------------------------
 java harjoitustyo.Oope2HT < esimerkit/in_ls2.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_ls2.txt -
 echo " "
 echo " "
 
-echo "-----------md1----------------"
+echo "-----------md1-----------------------------------------------------------------------------------------------"
 java harjoitustyo.Oope2HT < esimerkit/in_md1.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_md1.txt -
 echo " "
 echo " "
 
-echo "-----------md2----------------"
+echo "-----------md2-----------------------------------------------------------------------------------------------"
 java harjoitustyo.Oope2HT < esimerkit/in_md2.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_md2.txt -
 echo " "
 echo " "
 
-echo "-----------mf1----------------"
+echo "-----------mf1-----------------------------------------------------------------------------------------------"
 java harjoitustyo.Oope2HT < esimerkit/in_mf1.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_mf1.txt -
 echo " "
 echo " "
 
-echo "-----------mf2----------------"
+echo "-----------mf2-----------------------------------------------------------------------------------------------"
 java harjoitustyo.Oope2HT < esimerkit/in_mf2.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_mf2.txt -
 echo " "
 echo " "
 
-echo "-----------rm1----------------"
+echo "-----------rm1-----------------------------------------------------------------------------------------------"
 java harjoitustyo.Oope2HT < esimerkit/in_rm1.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_rm1.txt -
 echo " "
 echo " "
 
-echo "-----------rm2----------------"
+echo "-----------rm2-----------------------------------------------------------------------------------------------"
 java harjoitustyo.Oope2HT < esimerkit/in_rm2.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_rm2.txt -
 echo " "
 echo " "
 
-echo "-----------assignment----------------"
+echo "-----------assignment-----------------------------------------------------------------------------------------------"
 java harjoitustyo.Oope2HT < esimerkit/in_assignment.txt > out.txt  && 
 sed 's/>\([a-zA-Z0-9_./]\)/>\n\1/g' out.txt | diff -s --side-by-side --suppress-common-lines esimerkit/out_assignment.txt -

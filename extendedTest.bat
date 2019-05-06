36b6c9d8eb8846b7ba1b8bec47ec9afb384b45a3
Administrator <jaan.taponen@hotmail.com>
2019-05-06 17:00:04 +0300

    uudet testit (windows)

--- extendedTest.bat
+++ extendedTest.bat
@@ -1,62 +1,33 @@
-ECHO OFF
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-cd scriptsrc
-cd tulokset
+cd scriptsrc\tulokset
 DEL /F/Q/S *.* > NUL
 cd ..
 cd ..
-
 ECHO KAANNETAAN JAVA
 PowerShell javac .\harjoitustyo\Oope2HT.java
-ECHO KAANNETAAN scripti
 
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_cd1.txt > scriptsrc\tulokset\temp.txt   
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_cd1.txt
-
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_cd2.txt > scriptsrc\tulokset\temp.txt  
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_cd2.txt
-
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_exit.txt > scriptsrc\tulokset\temp.txt   
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_exit.txt
-
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_ls1.txt > scriptsrc\tulokset\temp.txt   
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_ls1.txt
-
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_ls2.txt > scriptsrc\tulokset\temp.txt  
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_ls2.txt
-
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_md1.txt > scriptsrc\tulokset\temp.txt   
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_md1.txt
-
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_md2.txt > scriptsrc\tulokset\temp.txt   
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_md2.txt
-
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_mf1.txt > scriptsrc\tulokset\temp.txt   
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_mf1.txt
-
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_mf2.txt > scriptsrc\tulokset\temp.txt  
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_mf2.txt
-
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_rm1.txt > scriptsrc\tulokset\temp.txt   
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_rm1.txt
-
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_rm2.txt > scriptsrc\tulokset\temp.txt  
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_rm2.txt
-
-java harjoitustyo.Oope2HT < scriptsrc\syotteet\in_assignment.txt > scriptsrc\tulokset\temp.txt   
-PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out_assignment.txt
-
-DEL scriptsrc\tulokset\temp.txt
-
-cd scriptsrc
+cd scriptsrc\syotteet
+for %%v in (*.txt) do (
+	ECHO %%v
+	cd ..
+	cd ..
+	java harjoitustyo.Oope2HT < scriptsrc\syotteet\%%v > scriptsrc\tulokset\temp.txt
+	SET _test=%%v
+	PowerShell "(Get-Content scriptsrc\tulokset\temp.txt) -replace '>([a-zA-Z0-9_./]+)', "">`r`n`$1""" > scriptsrc\tulokset\out!_test:~2!
+	cd scriptsrc\syotteet
+)
+cd ..
+cd tulokset
+DEL temp.txt
+cd ..
 
 ECHO -------------------------------------
 ECHO --Tulostetaan mahdolliset eriavaisyydet--
-ECHO 
 java -jar Vertailu.jar
+ECHO -------------------------------------
 cd ..\harjoitustyo
 DEL /F/Q/S *.class > NUL
-ECHO ---------------------------------------------------
-ECHO KASVATA KOMENTORIVIN KOKOA NIIN NAET RIVIT KUNNOLLA
-ECHO ---------------------------------------------------
+
 PAUSE
